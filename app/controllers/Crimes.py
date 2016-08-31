2999e48616000b1e4a525bb30bf6cef45aafe387
Brandon McMahan <brandonmdesign@gmail.com>
2016-08-31 16:52:41 -0700

    added buttons and ids for filtering crimes by category

--- app/controllers/Crimes.py
+++ app/controllers/Crimes.py
@@ -13,17 +13,17 @@ class Crimes(Controller):
         super(Crimes, self).__init__(action)
         self.load_model('Crime')
         self.db = self._app.db
-   
+
     def index(self):
         """
-        A loaded model is accessible through the models attribute 
+        A loaded model is accessible through the models attribute
         self.models['WelcomeModel'].get_users()
-        
+
         self.models['WelcomeModel'].add_message()
         # messages = self.models['WelcomeModel'].grab_messages()
         # user = self.models['WelcomeModel'].get_user()
         # to pass information on to a view it's the same as it was with Flask
-        
+
         # return self.load_view('index.html', messages=messages, user=user)
         """
         crimes=self.models['Crime'].get_crimes()
--- app/models/Crime.py
+++ app/models/Crime.py
@@ -11,7 +11,7 @@ class Crime(Model):
   def get_crimes(self):
     crimes = client.get("cuks-n6tp", select ="category,time,location,date", where=""" category='ASSAULT'
      or category='VEHICLE THEFT' or category='VANDALISM' or category='KIDNAPPING'
-     or category='SEX OFFENSES, FORCIBLE' or category='DRIVING UNDER THE INFLUENCE' and DATE > '2015-01-01T00:00:00.000' """,limit=30)
+     or category='SEX OFFENSES, FORCIBLE' or category='DRIVING UNDER THE INFLUENCE' and DATE > '2015-01-01T00:00:00.000' """,limit=70)
     for crime in crimes:
       real_time = time.strptime(crime['time'], "%H:%M")
       crime['real_time'] = real_time
